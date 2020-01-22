06cf076d889070a32c11ab4a706fa856c3178d0c
SathwikaGone <gonesathwika@gmail.com>
2020-01-22 16:18:54 -0500

    forth question

--- 01Basics/JavascriptExcercises.js
+++ 01Basics/JavascriptExcercises.js
@@ -23,3 +23,32 @@ diff(8);
  } 
  sum(5,7);
  sum(5,5);
+
+ /* Write a JavaScript program to compute the absolute difference between a specified number and 19.
+  Returns triple their absolute difference if the specified number is greater than 19. */
+
+  function diff(number){
+   if (number>19){ console.log( 3* (number-19))}
+   else{
+       console.log((19-number));
+   }
+   
+}
+diff(8);
+diff(29);
+
+/*Write a JavaScript program to create a new string adding "Py" in front of a given string.
+ If the given string begins with "Py" then return the original string. */
+
+ function py(name){
+    if (name.substring(0,2)==="py"){
+       console.log(name);
+    }
+    else{
+       var str="py";
+       
+       console.log(str.concat(name));
+    }
+ }
+ py("pyname");
+ py("sathwika");
